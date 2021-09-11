b953f7cdff6f0005d2b55df9fad22ff9cb8c4f73
mohitsethi <mohit@sethis.in>
2021-09-11 11:17:23 +0530

    updated spark-cluster

---
+++ spark/cluster/data/wordcount.py
@@ -0,0 +1,17 @@
+import sys
+ 
+from pyspark import SparkContext, SparkConf
+ 
+if __name__ == "__main__":
+	
+	# create Spark context with necessary configuration
+	sc = SparkContext("local","PySpark Word Count Exmaple")
+	
+	# read data from text file and split each line into words
+	words = sc.textFile("/appdata/input.txt").flatMap(lambda line: line.split(" "))
+	
+	# count the occurrence of each word
+	wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
+	
+	# save the counts to output
+	wordCounts.saveAsTextFile("/appdata/spark/output/")
