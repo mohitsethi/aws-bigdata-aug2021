9151ba44ddac73f192922982dc54eb509be55d2f
mohitsethi <mohit@sethis.in>
2021-09-12 08:01:31 +0530

    updated spark wc

--- spark/0-wordcount/before/WordCount/build.sbt
+++ spark/0-wordcount/before/WordCount/build.sbt
@@ -1,7 +1,6 @@
 name := "Word Count"
-
 version := "0.1"
-
 scalaVersion := "2.10.5"
-
 libraryDependencies += "org.apache.spark" % "spark-core_2.10" % "1.4.0" % "provided"
+resolvers += Resolver.url("maven_central", url("https://repo.maven.apache.org/maven2/"))
+resolvers += Resolver.url("maven_central", url("https://repo.maven.apache.org/maven2/"))
--- spark/0-wordcount/before/WordCount/src/main/scala/WordCounter.scala
+++ spark/0-wordcount/before/WordCount/src/main/scala/WordCounter.scala
@@ -7,11 +7,11 @@ object WordCounter {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Word Counter")
     val sc = new SparkContext(conf)
-    val textFile = sc.textFile("file:///Spark/README.md")
+    val textFile = sc.textFile("file:///appdata/input.txt")
     val tokenizedFileData = textFile.flatMap(line=>line.split(" "))
     val countPrep = tokenizedFileData.map(word=>(word, 1))
     val counts = countPrep.reduceByKey((accumValue, newValue)=>accumValue + newValue)
     val sortedCounts = counts.sortBy(kvPair=>kvPair._2, false)
-    sortedCounts.saveAsTextFile("file:///msData/ReadMeWordCountViaApp")
+    sortedCounts.saveAsTextFile("file:///appdata/spark/output")
   }
 }
